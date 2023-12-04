bc8faf207b7dc3e8c37b0e2bd44ce0adf8f5bd68
michaelp19 <pelletim@dickinson.edu>
2023-12-04 13:42:31 -0500

    Added R markdown

--- Final/Final.R
+++ Final/Final.R
@@ -1,5 +1,25 @@
+
+**Due date:** December 4 beginning of class.
+
+
+```{r echo=FALSE}
+# Custom options for knitting
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  error = FALSE, 
+  fig.align = "center",
+  cache = FALSE
+) 
+```
+
+
+In this homework, we will analyze news headlines data scraped from abcnews, posted on Github in a csv file named `news.csv`.
+
+Read the `news.csv` into R and create the object `news` using
+
 ```{r}
-library(tidyverse)
+#library(tidyverse)
 library(tm)
-news<-read.csv("/Users/michaelpelletier/Desktop/Data 180-Michael Pelletier version2 /DATA-180-Michael-Pelletier/hw7/news.csv",header=T)
+news<-read.csv("news.csv",header=T)
 ```
